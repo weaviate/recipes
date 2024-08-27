76124125cae00b5e0c5b06e6c25838cf9dd8a970
Connor Shorten <connorshorten300@gmail.com>
2024-08-27 12:06:44 -0400

    Create modal_web_server.py

---
+++ integrations/compute-infrastructure/modal/modal_web_server.py
@@ -0,0 +1,36 @@
+import modal
+import modal.gpu
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+
+# from worker.src.llm.handlers.modal.engine import Model, app
+from vllm_outlines_setup import Model, app
+
+MINUTE = 60
+
+web_image = modal.Image.debian_slim(python_version="3.10")
+
+auth_scheme = HTTPBearer()
+
+
+@app.function(
+    image=web_image,
+    # secrets=[modal.Secret.from_name("my-inference-secret")],
+    container_idle_timeout=MINUTE
+    * 20,  # keeps web container alive for 20 minutes (the max)
+)
+@modal.web_endpoint(method="POST")
+def generate_web(
+    data: dict, token: HTTPAuthorizationCredentials = Depends(auth_scheme)
+):
+    import os
+
+    '''
+    if token.credentials != os.environ["LLM_INFERENCE_KEY"]:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect bearer token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    '''
+    return Model.generate.remote_gen(data["prompts"], settings=None)
